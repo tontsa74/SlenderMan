a4351d8cacf83fe4aeb960b2813f2b1a361719fe
tontsa74 <toni.silventoinen@cs.tamk.fi>
2019-06-15 07:56:14 +0300

    add player move

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float speed = 5f;
+    public float rotationSpeed = 90;
+
+    Vector3 inputVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateInputVector();
+        Move();
+        Rotate();
+    }
+
+    void UpdateInputVector()
+    {
+        inputVector = new Vector3(Input.GetAxis("Horizontal"),
+                                    0,
+                                    Input.GetAxis("Vertical")).normalized;
+    }
+
+    void Move()
+    {
+        // transform.position += inputVector * Time.deltaTime * speed;
+
+        transform.Translate(inputVector * Time.deltaTime * speed);
+    }
+
+    void Rotate()
+    {
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime);
+    }
+}
