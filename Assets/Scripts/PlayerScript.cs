fe210bde0ac29b05c24772dc986cd3d3e89a5b1e
tontsa74 <toni.silventoinen@cs.tamk.fi>
2019-06-17 13:35:41 +0300

    akdha

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -20,7 +20,7 @@ public class PlayerScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         UpdateInputVector();
         Move();
